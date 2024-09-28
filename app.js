325804678feec2d73c51601be4626b9a61d1dc45
danmachooo <johnpauldanmachi@gmail.com>
2024-09-28 10:35:09 +0800

    Finished configuring the database

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const audioRoutes = require('./routes/audioRoute')
 const albumRoutes = require('./routes/albumRoute')
 const playlistRoutes = require('./routes/playlistRoute')
+const db = require('./config/db')
 const port = 3000;
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static('public'))
 
-app.use('/', audioRoutes);
-app.use('/albums', albumRoutes);
-app.use('/playlists', playlistRoutes);
+// app.use('/', audioRoutes);
+// app.use('/albums', albumRoutes);
+// app.use('/playlists', playlistRoutes);
 
-app.listen(port, () => {
-    console.log(`Connected: Running on http://localhost:${port}`);
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
+
+db.init_db()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Connected: Running on http://localhost:${port}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Database initialization failed', error);
+    });
